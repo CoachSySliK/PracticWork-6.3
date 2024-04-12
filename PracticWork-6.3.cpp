da945307db0b638c4fe27ec77a06834a2b9642d7
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-12 20:14:37 +0300

    Добавьте файлы проекта.

---
+++ PracticWork-6.3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string rub(int ending) {
+    if (ending % 10 == 0 || ending % 10 >= 5 || (ending / 10) % 10 == 1) {
+        return " рублей";
+    }
+    else if (ending % 10 == 1) {
+        return " рубль";
+    }
+    else {
+        return " рубля";
+    }
+}
+
+int main()
+{
+    string name;
+    cout << "Как Вас зовут? ";
+    cin >> name;
+
+    int account;    
+    int credit;
+    credit:
+    cout << name << ", введите сумму долга: ";
+    cin >> credit;
+    if (credit < 0) {
+        cout << "Ошибка!!! " << name << ", введите НЕ отрицательное значение!";
+        goto credit;
+    }
+    account = -credit;    
+
+    while (account < 0) {
+        cout << name << ", Вы должны " << -account << rub(-account) << endl;
+        int debit;
+        cout << name << " погасите долг!!!\nПополните счёт: ";
+        cin >> debit;
+        account += debit;
+    }
+
+    cout << name << ", Вы погасили долг!!!\nНа Вашем счёте осталось: " << account << rub(account) << endl;
+
+    return 0;
+}
